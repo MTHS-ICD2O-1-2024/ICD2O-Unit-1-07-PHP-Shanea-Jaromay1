327901c0beb16844cfa36aed9cb6120f1c65d1b0
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-02-26 18:15:03 +0000

    init

--- index.php
+++ index.php
@@ -1,9 +1,55 @@
 <!DOCTYPE html>
-  <html>
+<!-- ICS2O-Unit1-07-HTML-Favicon -->
+<html lang="en-ca">
   <head>
-    <title>Title</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="My first webpage, with a favicon" />
+    <meta name="keywords" content="mths, icd2o" />
+    <meta name="author" content="Shanea Jaromay" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/style.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
+    <link
+      rel="stylesheet"
+      href="https://code.getmdl.io/1.3.0/material.cyan-deep_purple.min.css"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="./images/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="./images/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="./images/favicon-16x16.png"
+    />
+    <link rel="manifest" href="./images/site.webmanifest" />
+    <title>My first webpage, with a favicon</title>
   </head>
   <body>
-    <?php echo '<p>Hello, World!, in PHP</p>'; ?>
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">Favicon</span>
+        </div>
+      </header>
+      <main class="mdl-layout__content">
+        <div class="page-content">See favicon above.</div>
+        <p>
+          <a href="./index2.html">My index2.html page</a>
+        </p>
+      </main>
+    </div>
   </body>
 </html>
