b9e3e98930b58b94e563cad789c15ba2a065125e
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-02-28 03:48:34 +0000

    init

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
-<!-- ICS2O-Unit1-07-HTML-Favicon -->
+<!-- ICS2O-Unit1-07-HTML-MDL-PHP -->
 <html lang="en-ca">
   <head>
     <meta charset="utf-8" />
-    <meta name="description" content="My first webpage, with a favicon" />
+    <meta name="description" content="Hello, World!, PHP" />
     <meta name="keywords" content="mths, icd2o" />
     <meta name="author" content="Shanea Jaromay" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./css/style.css" />
     <link
       rel="stylesheet"
       href="https://fonts.googleapis.com/icon?family=Material+Icons"
     />
     <link
       rel="stylesheet"
-      href="https://code.getmdl.io/1.3.0/material.cyan-deep_purple.min.css"
+      href="https://code.getmdl.io/1.3.0/material.orange-teal.min.css" />
     />
+    <link rel="stylesheet" href="./css/style.css" />
     <link
       rel="apple-touch-icon"
       sizes="180x180"
@@ -34,21 +34,27 @@
       href="./images/favicon-16x16.png"
     />
     <link rel="manifest" href="./images/site.webmanifest" />
-    <title>My first webpage, with a favicon</title>
+    <title>Hello, World!, PHP</title>
   </head>
+
   <body>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
       <header class="mdl-layout__header">
         <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">Favicon</span>
+          <span class="mdl-layout-title">Hello, World!, PHP</span>
         </div>
       </header>
       <main class="mdl-layout__content">
-        <div class="page-content">See favicon above.</div>
-        <p>
-          <a href="./index2.html">My index2.html page</a>
-        </p>
+        <div class="page-content">
+          <?php echo '<p>Hello, World!, in PHP</p>'; ?>
+        </div>
+        <img src="./images/notebook-animation.svg" />
+        <!-- Simple Textfield for integers-->
+        <form action="#">
+          <br />
+          <!-- Raised button with ripple -->
+        </form>
       </main>
     </div>
   </body>
